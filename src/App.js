025ad6dd86993c3e43fb0ff329cb6c030941d52a
Muhammed Haris (CW) <Muhammed.Haris@jda.com>
2024-03-06 12:54:03 +0530

    changed app with trainer title

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello I am Arjun</h1>
+      <h1>Hello I am Arjun(and trainer for now)</h1>
     </div>
   );
 }
